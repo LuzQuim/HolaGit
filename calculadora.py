47da2d2103c2b6acc64c5d5ec253136fd893cd78
LuzQuim <luzdaryquimbayo01@gmail.com>
2025-07-06 19:57:57 -0500

    Añado la funcion de restar

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,7 @@
 def sumar(a: float, b: float) -> float:
     #Suma dos números.
     return a + b
+
+def restar (a: float, b: float) -> float:
+    #Resta el segundo número al primero.
+    return a - b
